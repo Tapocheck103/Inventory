45c5d148eee9dc6ec63cfd42eb7a60fca5971267
Tapocheck103 <tapocheck103@gmail.com>
2025-08-14 02:55:58 +0500

    added all files

---
+++ src/main/java/com/tapocheck103/inventory/Inventory.java
@@ -0,0 +1,17 @@
+package com.tapocheck103.inventory;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Inventory extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
